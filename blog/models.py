27453160eac8c66fa6cf4e6389d2d868a07b8f6d
Zachary Cannon <zach.cannon10@gmail.com>
2013-11-16 21:27:09 -0600

    Added user model for database and can now view...

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,10 @@
 from google.appengine.ext import db
 
 class Post(db.Model):
-    title = db.StringProperty(required = True)
-    content = db.TextProperty(required = True)
-    when = db.DateTimeProperty(auto_now_add = True)
-    author = db.UserProperty(required = True)
+	title = db.StringProperty(required = True)
+	content = db.TextProperty(required = True)
+	when = db.DateTimeProperty(auto_now_add = True)
+	author = db.UserProperty(required = True)
+
+class RecommenderUser(db.Model):
+	username = db.StringProperty(required = True)
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from blog import app
 from models import Post
+from models import RecommenderUser
 from decorators import login_required
 
 from flask import render_template, flash, url_for, redirect
@@ -9,27 +10,37 @@ from flaskext.wtf import validators
 from google.appengine.api import users
 
 class PostForm(wtf.Form):
-    title = wtf.TextField('Title', validators=[validators.Required()])
-    content = wtf.TextAreaField('Content', validators=[validators.Required()])
+	title = wtf.TextField('Title', validators=[validators.Required()])
+	content = wtf.TextAreaField('Content', validators=[validators.Required()])
+	
+class AddUserForm(wtf.Form):
+	username = wtf.TextField('Name', validators=[validators.Required()])
 
+#Default pages/routes with template
 @app.route('/')
 def redirect_to_home():
-    return redirect(url_for('list_posts'))
-
+	return redirect(url_for('list_posts'))
+	
 @app.route('/posts')
 def list_posts():
-    posts = Post.all()
-    return render_template('list_posts.html', posts=posts)
+	posts = Post.all()
+	return render_template('list_posts.html', posts=posts)
 
 @app.route('/posts/new', methods = ['GET', 'POST'])
 @login_required
 def new_post():
-    form = PostForm()
-    if form.validate_on_submit():
-        post = Post(title = form.title.data,
-                    content = form.content.data,
-                    author = users.get_current_user())
-        post.put()
-        flash('Post saved on database.')
-        return redirect(url_for('list_posts'))
-    return render_template('new_post.html', form=form)
+	form = PostForm()
+	if form.validate_on_submit():
+		post = Post(title = form.title.data,
+					content = form.content.data,
+					author = users.get_current_user())
+		post.put()
+		flash('Post saved on database.')
+		return redirect(url_for('list_posts'))
+	return render_template('new_post.html', form=form)
+
+#Added pages/routes for TNGA
+@app.route('/list_users')
+def list_users():
+	recommender_users = RecommenderUser.all()
+	return render_template('list_recommender_users.html', recommender_users = recommender_users)
